f2cd5604e4035b13b0a7d6ddcb8a2a7a139826ae
Carey <944131226@qq.com>
2018-10-31 23:55:18 +0800

    修改用户查询 cmd/qryuser.go update

--- cmd/qryuser.go
+++ cmd/qryuser.go
@@ -23,13 +23,10 @@ import (
 // qryuserCmd represents the qryuser command
 var qryuserCmd = &cobra.Command{
 	Use:   "qryuser",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "用户查询",
+	Long: `
+GoAgenda qryuser
+	`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("qryuser called")
 	},
