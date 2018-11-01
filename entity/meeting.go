263efbc4beb3054b66ef33ebfa4165974ca08ffb
chenf99 <1163874966@qq.com>
2018-11-01 20:30:47 +0800

    增加service层，分离试题逻辑

--- entity/meeting.go
+++ entity/meeting.go
@@ -1,1 +1,59 @@
 package entity
+
+//会议信息数据结构
+type MeetingData struct{
+	Title 		 string 	`json:"title"`
+	Sponsor 	 string 	`json:"sponsor"`
+	Participator []string   `json:"participator"`
+	Start 		 string 	`json:"start"`
+	End 		 string 	`json:"end"`
+}
+
+//得到会议发起者
+func (m *MeetingData) GetSponsor() string{
+	return m.Sponsor
+}
+
+//得到会议参与者
+func (m *MeetingData) GetParticipator() []string{
+	return m.Participator
+}
+
+//增加会议参与者
+func (m *MeetingData) AddParticipator(participator string) {
+	m.Participator = append(m.Participator, participator)
+}
+
+//删除会议参与者
+func (m *MeetingData) RemoveParticipator(participator string) {
+	for i, meeting := range m.Participator {
+		if meeting == participator {
+			m.Participator = append(m.Participator[:i], m.Participator[i+1:]...)
+		}
+	}
+}
+
+//获得开始时间
+func (m *MeetingData) GetStartDate() string{
+	return m.Start
+}
+
+//获得结束时间
+func (m *MeetingData) GetEndDate() string{
+	return m.End
+}
+
+//获得会议标题
+func (m *MeetingData) GetTitle() string{
+	return m.Title
+}
+
+//判断用户是否参与会议
+func (m *MeetingData) IsParticipator(username string) bool{
+	for _, meeting := range m.Participator {
+		if meeting == username {
+			return true
+		}
+	}
+	return false
+}
---
+++ service/user.go
@@ -0,0 +1,118 @@
+package service
+
+import (
+	"os"
+	"github.com/chenf99/GoAgenda/entity"
+	"log"
+	"encoding/json"
+)
+
+type UserData entity.UserData
+
+//用户数据结构
+type UserList struct{
+	Users []UserData `json:"users"`
+}
+
+
+var UserModel UserList
+var userfile string = os.Getenv("GOPATH") + "/src/github.com/chenf99/GoAgenda/data/users.json"
+
+func init() {
+	UserModel.readFromFile()
+}
+
+//通过名字获得用户
+func (u *UserList)GetUserByName(username string) UserData{
+	for _, user := range u.Users {
+		if user.Name == username {
+			return user
+		}
+	}
+	return UserData{}
+}
+
+//获取所有用户
+func (u *UserList)GetAllUsers() []UserData{
+	return u.Users
+}
+
+//用户是否存在
+func (u *UserList)IsExist(username string) bool{
+	for _, user := range u.Users {
+		if user.Name == username {
+			return true
+		}
+	}
+	return false
+}
+
+//密码是否正确
+func (u *UserList)MatchPass(username, password string) bool{
+	for _, user := range u.Users {
+		if user.Name == username {
+			return user.Password == password
+		}
+	}
+	return false
+}
+
+//添加一个用户
+func (u *UserList)AddUser(userinfo UserData) {
+	u.Users = append(u.Users, userinfo)
+	u.saveToFile()
+}
+
+//删除一个用户
+func (u *UserList)DeleteUser(username string) bool{
+	for i, user := range u.Users {
+		if user.Name == username {
+			u.Users = append(u.Users[:i], u.Users[i+1:]...)
+			u.saveToFile()
+			return true
+		}
+	}
+	return false
+}
+
+func (u *UserList)saveToFile() {
+	//UserList转json格式数据
+	data, err := json.Marshal(*u)
+	if (err != nil) {
+		log.Fatal(err)
+	}
+	fp, err := os.OpenFile(userfile, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0755)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//写入文件
+	_, err = fp.Write(data)
+	defer fp.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func (u *UserList)readFromFile() {
+	//判断文件是否存在
+	_, err := os.Stat(userfile)
+	if os.IsNotExist(err) {
+		return 
+	}
+	fp, err := os.OpenFile(userfile, os.O_RDONLY, 0755)
+	defer fp.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	data := make([]byte, 1000)
+	//读取文件
+	total, err := fp.Read(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//解析json数据到UserList
+	err = json.Unmarshal(data[:total], u)
+	if err != nil {
+        log.Fatal(err)
+	}
+}
