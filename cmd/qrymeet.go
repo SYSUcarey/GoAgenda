5cf576b277b1c568c772afc8e1ec2267ed653c68
hath <772840087@qq.com>
2018-11-02 09:49:48 +0800

    更新: 查询会议

--- cmd/qrymeet.go
+++ cmd/qrymeet.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"fmt"
-
+	"github.com/chenf99/GoAgenda/entity"
+	"time"
+	"github.com/chenf99/GoAgenda/service"
 	"github.com/spf13/cobra"
 )
 
@@ -32,6 +34,68 @@ GoAgenda qrymeet -s starttime -e endtime
 			return
 		}
 		fmt.Println("endtime: " + endtime)
+
+		/*
+		 * 合法性判断
+		 * 1.是否登录
+		 * 2.时间格式是否合法
+		 * 3.开始时间是否小于结束时间   
+		 * 4.开始时间大于当前时间 
+		 */		 
+
+		curStatus := entity.CurStatus.GetStatus()
+
+		// 1.是否登录
+		has_login := curStatus.Islogin
+		if !has_login {
+			fmt.Println("GoAgenda qrymeet failed: You did not login yet!")
+			return
+		}
+
+		// 2.时间格式是否合法
+		start, err_starttime_format_invalid := time.Parse("2006-01-02/15:04:05", starttime)
+		if err_starttime_format_invalid != nil {
+			fmt.Println("GoAgenda qrymeet failed: starttime invalid!")
+			fmt.Println("GoAgenda qrymeet: starttime must be in format \"2006-01-02/15:04:05\"!")
+			fmt.Println(err_starttime_format_invalid)
+			return
+		}
+
+		end, err_endtime_format_invalid := time.Parse("2006-01-02/15:04:05", endtime)
+		if err_endtime_format_invalid != nil {
+			fmt.Println("GoAgenda qrymeet failed: endtime invalid!")
+			fmt.Println("GoAgenda qrymeet: endtime must be in format \"2006-01-02/15:04:05\"!")
+			fmt.Println(err_endtime_format_invalid)
+			return
+		}
+
+		// 3.开始时间是否小于结束时间
+		is_endtime_after_starttime := end.After(start)
+		if !is_endtime_after_starttime {
+			fmt.Println("GoAgenda qrymeet failed: endtime must be after starttime!")
+			return
+		}
+
+		// 4.开始时间大于当前时间
+		currenttime := time.Now()
+		is_starttime_after_currenttime := start.After(currenttime)
+		if !is_starttime_after_currenttime {
+			fmt.Println("GoAgenda qrymeet failed: starttime must be after current time!")
+			return
+		}
+
+		// 参数合法
+		fmt.Println("GoAgenda qrymeet succeed")
+		meetingList := service.MeetingModel.MeetingQuery(curStatus.UserName, starttime, endtime)
+		fmt.Println("There are ", len(meetingList), "meeting you sponsor or participate:")
+		for i,meeting := range meetingList {
+			fmt.Println("Meeting ", i + 1)
+			fmt.Println("Title: ",meeting.GetTitle())
+			fmt.Println("Sponsor: ",meeting.GetSponsor())
+			fmt.Println("Participators: ",meeting.GetParticipator())
+			fmt.Println("Start: ",meeting.GetStartDate())
+			fmt.Println("End: ",meeting.GetEndDate())
+		}
 	},
 }
 
